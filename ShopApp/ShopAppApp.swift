8b1aa8ef2ff05ddb71af5052ba68b08ed7ed651b
fady <fady>
2022-06-02 04:09:44 +0200

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Folio on 02/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
